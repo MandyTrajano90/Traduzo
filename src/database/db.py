f4d410367b2fd4a95de811fd1b5b8398fce438c3
Mandytrajano90 <mandytrajano90@gmail.com>
2024-05-15 19:24:12 -0300

    Req 1 - conection with Mongo

--- src/database/db.py
+++ src/database/db.py
@@ -1,1 +1,9 @@
-#Starting
+from pymongo import MongoClient
+import os
+
+conection = os.environ.get('MONGO_URI', 'mongodb://localhost:27017/')
+client = MongoClient(conection)
+
+db_client = os.environ.get('DB_NAME', 'test_db_traduzo')
+
+db = client[db_client]
