19572fca3e3172d2256fa55bfcb77eadf9ea36e4
Mandytrajano90 <mandytrajano90@gmail.com>
2024-05-18 13:03:37 -0300

    Req 6 - enpoint POST

--- src/app.py
+++ src/app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from deep_translator import GoogleTranslator
 from models.language_model import LanguageModel
 
 from controllers.admin_controller import admin_controller
@@ -14,7 +15,7 @@ app.static_folder = "views/static"
 app.register_blueprint(admin_controller, url_prefix="/admin")
 
 
-@app.route("/")
+@app.route("/", methods=["GET"])
 def index():
     return render_template(
         "index.html",
@@ -23,6 +24,26 @@ def index():
         translate_from="pt",
         translate_to="en",
         translated="What do you want to translate?",
+    )
+
+
+@app.route("/", methods=["POST"])
+def translate():
+    text_to_translate = request.form.get('text-to-translate')
+    translate_from = request.form.get('translate-from')
+    translate_to = request.form.get('translate-to')
+
+    translated = GoogleTranslator(
+        source=translate_from, target=translate_to
+    ).translate(text_to_translate)
+
+    return render_template(
+        "index.html",
+        languages=LanguageModel.list_dicts(),
+        text_to_translate=text_to_translate,
+        translate_from=translate_from,
+        translate_to=translate_to,
+        translated=translated,
     )
 
 
